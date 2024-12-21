701d1a55930f9e5390a1ca46e2622be5d09cdc19
DongSeonYoo <inko51366@naver.com>
2024-12-21 18:39:35 +0000

    feat: add a condition to checkt if the enviroment is 'development' in prismaservice

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) {
+    super(
+      configService.get<string>('NODE_ENV') === 'development'
+        ? {}
+        : {
+            log: ['query'],
+          },
+    );
+  }
   async onModuleInit() {
     await this.$connect();
   }
