fcb97ec47bac0c16846c294a189164e128b622e5
DongSeonYoo <inko51366@naver.com>
2024-12-21 18:36:49 +0000

    refactor: remove necessary codes

--- src/apis/users/users.module.ts
+++ src/apis/users/users.module.ts
@@ -3,17 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { JwtAccessStrategy } from '../auth/strategy/jwt.strategy';
 import { AuthModule } from '../auth/auth.module';
-import { UsersRepository } from './users.repository';
-import { TransactionManager } from 'src/prisma/prisma-transaction.manager';
 
 @Module({
   imports: [AuthModule],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    JwtAccessStrategy,
-    UsersRepository,
-    TransactionManager,
-  ],
+  providers: [UsersService, JwtAccessStrategy],
 })
 export class UsersModule {}
