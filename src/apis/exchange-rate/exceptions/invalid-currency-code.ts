f471a81d73813f5a61c0c50dce5e523f45ba2226
DongSeonYoo <inko51366@naver.com>
2024-12-30 21:43:35 +0000

    refactor: modify invalidCurrencyCodeException exception

--- src/apis/exchange-rate/exceptions/invalid-currency-code.ts
+++ src/apis/exchange-rate/exceptions/invalid-currency-code.ts
@@ -1,7 +1,7 @@
 import { BadRequestException } from '@nestjs/common';
 
 export class InvalidCurrencyCodeException extends BadRequestException {
-  constructor(message: string = '올바르지 않은 통화 코드입니다') {
-    super(message);
+  constructor(invalidCode: string = '[propertyName]=propertyValue') {
+    super(`올바르지 않은 통화 코드입니다: ${invalidCode}`);
   }
 }
