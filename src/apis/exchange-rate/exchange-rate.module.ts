c0c35ec1863e0f0a1a5c892a0f26df1ab81e914f
DongSeonYoo <inko51366@naver.com>
2025-01-07 20:39:15 +0000

    feat: add saveLatestRates method

--- src/apis/exchange-rate/exchange-rate.module.ts
+++ src/apis/exchange-rate/exchange-rate.module.ts
@@ -4,10 +4,17 @@ import { ExchangeRateController } from './exchange-rate.controller';
 import { FixerModule } from '../fixer/fixer.module';
 import { RedisModule } from '../../redis/redis.module';
 import { DateUtilModule } from '../../utils/date-util/date-util.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ExchageRateScheduler } from './schedulers/exchange-rate.scheduler';
+import { ExchangeRateRepository } from './exchange-rate.repository';
 
 @Module({
-  imports: [FixerModule, RedisModule, DateUtilModule],
+  imports: [FixerModule, RedisModule, DateUtilModule, ScheduleModule.forRoot()],
   controllers: [ExchangeRateController],
-  providers: [ExchangeRateService],
+  providers: [
+    ExchangeRateService,
+    ExchageRateScheduler,
+    ExchangeRateRepository,
+  ],
 })
 export class ExchangeRateModule {}
--- src/apis/exchange-rate/exchange-rate.service.ts
+++ src/apis/exchange-rate/exchange-rate.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { RedisService } from '../../redis/redis.service';
 import { MockFixerService } from '../fixer/mock/fixer-mock.service';
-import { DateUtilService } from '../../utils/date-util/date-util.service';
+import { ExchangeRateRepository } from './exchange-rate.repository';
+import { IExchangeRate } from './interface/exchange-rate.interface';
+import { RedisService } from '../../redis/redis.service';
 
 @Injectable()
 export class ExchangeRateService {
   constructor(
     private readonly fixerService: MockFixerService,
+    private readonly exchangeRateRepository: ExchangeRateRepository,
     private readonly redisService: RedisService,
-    private readonly dateUtilService: DateUtilService,
   ) {}
 
   async getCurrencyExchangeRates(
@@ -24,5 +25,30 @@ export class ExchangeRateService {
       latestRates,
       fluctuationRates,
     };
+  }
+
+  /**
+   * Performing tasks following caching strategies
+   *
+   * 1. get latest-rate from external API
+   * 2. convert recived data to record for RDB
+   * 3. update cache
+   */
+  async saveLatestRates(): Promise<void> {
+    const rates = await this.fixerService.getLatestRates();
+    const baseCurrency = rates.base;
+    const records: IExchangeRate.ICreateMany = Object.entries(rates.rates).map(
+      ([currencyCode, rate]) => ({
+        baseCurrency,
+        currencyCode,
+        rate,
+      }),
+    );
+
+    // TODO: distribute transaction & analize excution time
+    await Promise.all([
+      this.exchangeRateRepository.intertMany(records),
+      this.redisService.updateLatestRateCache(records),
+    ]);
   }
 }
