6779c8457db6cf2ac600349b0b4fe864d2fc2acb
DongSeonYoo <inko51366@naver.com>
2025-01-26 15:21:52 +0000

    feat: add price-notification api endpoint

--- src/apis/price-alert/price-notification.controller.ts
+++ src/apis/price-alert/price-notification.controller.ts
@@ -1,11 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { PriceNotificationService } from './price-notification.service';
 
 @Controller('price-notification')
 export class PriceNotificationController {
-  constructor(private readonly priceAlertService: PriceNotificationService) {}
+  constructor(
+    private readonly priceNotificationService: PriceNotificationService,
+  ) {}
 
   /**
-   * Create Notification
+   * 알림 생성
    */
+  @Post()
+  async createPriceNotification() {}
+
+  /**
+   * 알림 목록 조회
+   */
+  @Get()
+  async getNotificationList() {}
+
+  /**
+   * 알림 수정
+   */
+  @Put(':idx')
+  async updateNotification() {}
+
+  /**
+   * 알림 삭제
+   */
+  @Delete(':idx')
+  async deleteNotification() {}
 }
