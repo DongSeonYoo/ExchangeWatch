5dea1d9624e4e19b9591e6dcc833489e0e0f525a
DongSeonYoo <inko51366@naver.com>
2025-01-29 16:20:11 +0000

    refactor: rename jwt-guard to jwt-access-guard && separate JwtAuthException

--- src/decorators/jwt-auth.decorator.ts
+++ src/decorators/jwt-auth.decorator.ts
@@ -1,13 +1,13 @@
 import { UseGuards, applyDecorators } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { ApiExceptions } from './swaggers/exception.decorator';
-import { JwtAuthGuard } from 'src/apis/auth/guards/jwt.guard';
+import { JwtAccessGuard } from 'src/apis/auth/guards/jwt-access.guard';
 import { JwtAuthException } from 'src/apis/auth/exceptions/jwt-auth-exception';
 
 export const LoginAuth = () => {
   return applyDecorators(
     ApiBearerAuth('access-token'),
-    UseGuards(JwtAuthGuard),
+    UseGuards(JwtAccessGuard),
     ApiExceptions({
       exampleTitle: 'accessToken이 올바르지 않은 경우',
       schema: JwtAuthException,
