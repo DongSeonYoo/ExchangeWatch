65729df08fcc0fbd75357de486f97fec2fb1638c
DongSeonYoo <inko51366@naver.com>
2025-01-29 16:40:52 +0000

    refactor: change the response message to be assigned to swagger

--- src/decorators/jwt-auth.decorator.ts
+++ src/decorators/jwt-auth.decorator.ts
@@ -2,15 +2,31 @@ import { UseGuards, applyDecorators } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { ApiExceptions } from './swaggers/exception.decorator';
 import { JwtAccessGuard } from 'src/apis/auth/guards/jwt-access.guard';
-import { JwtAuthException } from 'src/apis/auth/exceptions/jwt-auth-exception';
+import {
+  JwtAuthException,
+  JwtErrorType,
+} from 'src/apis/auth/exceptions/jwt-auth-exception';
 
 export const LoginAuth = () => {
   return applyDecorators(
     ApiBearerAuth('access-token'),
     UseGuards(JwtAccessGuard),
-    ApiExceptions({
-      exampleTitle: 'accessToken이 올바르지 않은 경우',
-      schema: JwtAuthException,
-    }),
+    ApiExceptions(
+      {
+        exampleTitle: 'accessToken이 올바르지 않은 경우',
+        schema: JwtAuthException,
+        message: 'INVALID_TOKEN' as JwtErrorType,
+      },
+      {
+        exampleTitle: 'accessToken이 만료되었을 경우',
+        schema: JwtAuthException,
+        message: 'TOKEN_EXPIRED' as JwtErrorType,
+      },
+      {
+        exampleTitle: '존재하지 않는 유저일 경우',
+        schema: JwtAuthException,
+        message: 'UNAUTHORIZED' as JwtErrorType,
+      },
+    ),
   );
 };
