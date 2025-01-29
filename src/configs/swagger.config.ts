8e61e061cbd824417b29e9293c8a4a2aed85b954
DongSeonYoo <inko51366@naver.com>
2025-01-29 15:43:21 +0000

    feat: add BearerAuth options to swagger

--- src/configs/swagger.config.ts
+++ src/configs/swagger.config.ts
@@ -6,6 +6,17 @@ export const setSwagger = (app: INestApplication) => {
     .setTitle('Exchange-rate')
     .setDescription('')
     .setVersion('0.0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
