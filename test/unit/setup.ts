ec9a1f3fbd7172a545fdae8a78259ca0335dba27
DongSeonYoo <inko51366@naver.com>
2025-02-06 21:03:58 +0000

    test: prepare external library mocking for each unit test

--- test/unit/setup.ts
+++ test/unit/setup.ts
@@ -1,1 +1,15 @@
 import 'reflect-metadata';
+import { IExchangeRateAPIService } from '../../src/externals/exchange-rates/interfaces/exchange-rate-api-service';
+
+// Mocking @nestjs-cls module
+jest.mock('@nestjs-cls/transactional', () => ({
+  ...jest.requireActual('@nestjs-cls/transactional'),
+  Transactional: () => () => {},
+}));
+
+export const mockExchangeRateAPI: IExchangeRateAPIService = {
+  getLatestRates: jest.fn(),
+  getHistoricalRates: jest.fn(),
+  getOHLCData: jest.fn(),
+  getTimeSeriesData: jest.fn(),
+};
