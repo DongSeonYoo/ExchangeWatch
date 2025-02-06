28ba549d80404e536216ae1105181bdbc74c6a68
DongSeonYoo <inko51366@naver.com>
2025-02-06 21:06:39 +0000

    test: create a module factory util class for integrated testing

---
+++ test/integration/utils/integrate-module.util.ts
@@ -0,0 +1,11 @@
+import { TestConfigModule } from '../modules/test-config.module';
+import { TestClsModule } from '../modules/test-cls.module';
+import { TestPrismaModule } from '../modules/test-prisma.module';
+import { TestRedisModule } from '../modules/test-redis.module';
+import { DynamicModule, Type } from '@nestjs/common';
+
+export class TestIntegrateModules {
+  static create(): (Type<any> | DynamicModule)[] {
+    return [TestConfigModule, TestClsModule, TestPrismaModule, TestRedisModule];
+  }
+}
