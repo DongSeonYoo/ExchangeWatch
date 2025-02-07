3ca226dce1446e2a66b1adb62d0491cada75b1e7
DongSeonYoo <inko51366@naver.com>
2025-02-07 23:53:07 +0000

    test: append enviroment variable for generate token for test

--- test/integration/config/test.config.ts
+++ test/integration/config/test.config.ts
@@ -10,10 +10,17 @@ export const testConfiguration = () => ({
     password: process.env.REDIS_PASSWORD,
   },
   token: {
-    JWT_ACCESS_SECRET: 'test_access_secret',
-    JWT_REFRESH_SECRET: 'test_refresh_secret',
-    ACCESS_TOKEN_EXPIRES_IN: '15m',
-    REFRESH_TOKEN_EXPIRES_IN: '7d',
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
+
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    REFRESH_TOKEN_TTL: process.env.REFRESH_TOKEN_TTL,
+  },
+  google: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
   },
 
   google: {
