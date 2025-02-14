207f77194d5ba33afcc18667b1f8c6e165811faa
DongSeonYoo <inko51366@naver.com>
2025-02-14 12:14:34 +0000

    refactor: rapping redis client

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,50 +1,64 @@
-import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, Inject, OnModuleDestroy } from '@nestjs/common';
 import { Redis } from 'ioredis';
-import { IExchangeRate } from '../apis/exchange-rate/interface/exchange-rate.interface';
-import { LatestRateCache } from './interface/currency-rate.interface';
 
 @Injectable()
-export class RedisService implements OnModuleInit {
-  private readonly CACHE_TTL = {
-    CURRENT_RATES: this.configService.get<number>('LATEST_CURRENCY_CACHE_TTL'),
-  };
+export class RedisService implements OnModuleDestroy {
+  constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
 
-  constructor(
-    @Inject('REDIS_CLIENT') private readonly redisClient: Redis,
-    private readonly logger: Logger,
-    private readonly configService: ConfigService,
-  ) {}
-
-  /**
-   * Update real-time currency rate to the redis
-   */
-  async updateLatestRateCache(
-    rates: IExchangeRate.ICreate[],
-    ttl = this.CACHE_TTL.CURRENT_RATES,
-  ) {
-    await Promise.all(
-      rates.map((rate) =>
-        this.redisClient.set(
-          `rate:${rate.baseCurrency}:${rate.currencyCode}`,
-          JSON.stringify(<LatestRateCache>{
-            rate: rate.rate,
-            updatedAt: new Date(),
-          }),
-          'EX',
-          Number(ttl),
-        ),
-      ),
-    );
+  onModuleDestroy() {
+    this.redisClient.quit();
   }
 
-  onModuleInit() {
-    this.redisClient.on('ready', () => {
-      this.logger.log('Redis client is ready.');
-    });
+  /**
+   * Redis default get
+   * @param key
+   * @returns
+   */
+  async get<T>(key: string): Promise<T | null> {
+    this.redisClient.get;
+    const rawData = await this.redisClient.get(key);
+    return rawData ? (JSON.parse(rawData) as T) : null;
+  }
 
-    this.redisClient.on('error', (error) => {
-      this.logger.error(`Redis client error: ${error}`);
-    });
+  /**
+   * Redis default set (with TTL)
+   * @param key
+   * @param value
+   * @param ttl
+   */
+  async set<T>(key: string, value: T, ttl?: number): Promise<void> {
+    const data = JSON.stringify(value);
+    ttl
+      ? await this.redisClient.set(key, data, 'EX', ttl)
+      : await this.redisClient.set(key, data);
+  }
+
+  /**
+   * Redis default del
+   * @param key
+   */
+  async del(key: string): Promise<void> {
+    await this.redisClient.del(key);
+  }
+
+  /**
+   * Redis batch set 'pipeline'
+   * @param commands
+   */
+  async batchSet(commands: [string, string, 'EX', number][]) {
+    const pipeline = this.redisClient.pipeline();
+    commands.forEach((cmd) => pipeline.set(...cmd));
+    await pipeline.exec();
+  }
+
+  /**
+   * Redis batch set 'pipeline'
+   * @param keys
+   */
+  async batchGet<T>(keys: string[]): Promise<T[]> {
+    const rawData = await this.redisClient.mget(keys);
+    return rawData
+      .map((data) => (data ? JSON.parse(data) : null))
+      .filter((item) => item !== null) as T[];
   }
 }
