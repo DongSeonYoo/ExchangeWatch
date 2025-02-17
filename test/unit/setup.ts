080a620b2716296c5d1acc91f600db4ac953cd9e
DongSeonYoo <inko51366@naver.com>
2025-02-17 16:37:24 +0000

    test: remove mock setting for unit test

--- test/unit/setup.ts
+++ test/unit/setup.ts
@@ -1,15 +1,7 @@
 import 'reflect-metadata';
-import { IExchangeRateAPIService } from '../../src/externals/exchange-rates/interfaces/exchange-rate-api-service';
 
 // Mocking @nestjs-cls module
 jest.mock('@nestjs-cls/transactional', () => ({
   ...jest.requireActual('@nestjs-cls/transactional'),
   Transactional: () => () => {},
 }));
-
-export const mockExchangeRateAPI: IExchangeRateAPIService = {
-  getLatestRates: jest.fn(),
-  getHistoricalRates: jest.fn(),
-  getFluctuationData: jest.fn(),
-  getTimeSeriesData: jest.fn(),
-};
