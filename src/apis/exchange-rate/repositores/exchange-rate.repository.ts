0d548dbe650f124747c6f5c3ba98256dfebb34b2
DongSeonYoo <inko51366@naver.com>
2025-02-22 17:57:38 +0000

    refactor: remove findDailyStats

--- src/apis/exchange-rate/repositores/exchange-rate.repository.ts
+++ src/apis/exchange-rate/repositores/exchange-rate.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { IExchangeRate } from '../interface/exchange-rate.interface';
-import { ExchangeRateDailyStasEntity } from '../entitites/exchange-rate-daily-statistics.entity';
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { ExchangeRatesEntity } from '../entitites/exchange-rate.entity';
@@ -17,44 +16,6 @@ export class ExchangeRateRepository {
     });
 
     return;
-  }
-
-  async findDailyStats(
-    startDate: Date,
-    endDate: Date,
-  ): Promise<ExchangeRateDailyStasEntity[]> {
-    const result = await this.txHost.tx.exchangeRates.groupBy({
-      by: ['baseCurrency', 'currencyCode'],
-      where: {
-        createdAt: {
-          gte: startDate,
-          lte: endDate,
-        },
-      },
-      _max: {
-        rate: true,
-      },
-      _min: {
-        rate: true,
-      },
-      _avg: {
-        rate: true,
-      },
-      _count: {
-        rate: true,
-      },
-    });
-
-    return result.map((e) => {
-      return ExchangeRateDailyStasEntity.from({
-        currencyCode: e.currencyCode,
-        baseCurrency: e.baseCurrency,
-        maxRate: e._max.rate!.toNumber(),
-        minRate: e._min.rate!.toNumber(),
-        avgRate: e._avg.rate!.toNumber(),
-        count: e._count.rate,
-      });
-    });
   }
 
   async findRatesByDate(
