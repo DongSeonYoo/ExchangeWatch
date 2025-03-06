1751a57a9490b8c41ee1f42b5c0be1e8d1f46c3a
DongSeonYoo <inko51366@naver.com>
2025-03-06 16:04:55 +0000

    test: add coinapi service unit test

--- src/externals/exchange-rates/coin-api/__test__/coin-api.service.spec.ts
+++ src/externals/exchange-rates/coin-api/__test__/coin-api.service.spec.ts
@@ -1,3 +1,41 @@
+import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
+import { CoinApiService } from '../coin-api.service';
+import { HttpService } from '@nestjs/axios';
+import { mock, MockProxy } from 'jest-mock-extended';
 
-describe('CoinApiService (unit, external)', () => {});
+describe('CoinApiService (unit, external)', () => {
+  let coinApiService: MockProxy<CoinApiService>;
+  let httpService: MockProxy<HttpService>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        CoinApiService,
+        {
+          provide: ConfigService,
+          useFactory: () => mock<CoinApiService>(),
+        },
+        {
+          provide: HttpService,
+          useFactory: () => mock<HttpService>(),
+        },
+      ],
+    }).compile();
+
+    coinApiService = module.get(CoinApiService);
+    httpService = module.get(HttpService);
+  });
+
+  it('Should be definded', () => {
+    expect(coinApiService).toBeDefined();
+  });
+
+  describe('getLatestRates', () => {
+    it('should return latest exchange rates', async () => {
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+});
