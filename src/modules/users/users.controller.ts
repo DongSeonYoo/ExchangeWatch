04bbe6846c4c4dc4241922ec90b68471116bda07
DongSeonYoo <inko51366@naver.com>
2025-04-02 16:31:39 +0100

    feat: add GET user profile API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiTags } from '@nestjs/swagger';
+import { ApiSuccess } from '../../common/decorators/swaggers/success.decorator';
+import { AccessAuth } from '../../common/decorators/swaggers/login-auth.decorator';
+import { UserProfileResDto } from './dto/user-profile.dto';
+import { LoggedInUser } from './decorator/logged-in-user.decorator';
+import { UserEntity } from './entities/user.entity';
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  /**
+   * 사용자 프로필 조회
+   *
+   * @remarks 사용자의 프로필 정보를 조회합니다
+   */
+  @Get('profile')
+  @ApiSuccess(UserProfileResDto)
+  @AccessAuth()
+  async getUserProfile(@LoggedInUser() user: UserEntity) {
+    const userProfile = await this.usersService.findUserByIdx(user.idx);
+
+    return UserProfileResDto.from(userProfile);
+  }
 }
