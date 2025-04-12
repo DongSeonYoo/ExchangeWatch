42a48d5e125a5771491b0d072f510856b94df8ae
DongSeonYoo <inko51366@naver.com>
2025-04-12 21:49:04 +0100

    feat: add createNotificationHistory method when triggered notification

--- src/modules/notification-histories/repositories/notification-history.repository.ts
+++ src/modules/notification-histories/repositories/notification-history.repository.ts
@@ -1,10 +1,28 @@
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Injectable } from '@nestjs/common';
+import {
+  NotificationDataMap,
+  NotificationType,
+} from '../../notifications/types/notification.type';
 
 @Injectable()
 export class NotificationHistoryRepository {
   constructor(
     private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
   ) {}
+
+  async createNotificationHistory<T extends NotificationType>(
+    userIdx: number,
+    notificationIdx: string,
+    notificationData: NotificationDataMap[T],
+  ) {
+    const result = await this.txHost.tx.notificationsHistories.create({
+      data: {
+        notificationIdx,
+        userIdx,
+        details: notificationData,
+      },
+    });
+  }
 }
