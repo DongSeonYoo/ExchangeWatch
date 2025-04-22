e0c811941cd854f23eea0b435b1a1d4bcb72af38
DongSeonYoo <inko51366@naver.com>
2025-04-22 23:10:50 +0100

    refactor: separate of dependnecies for each responsibility

--- src/infrastructure/externals/exchange-rates/market-status-scheduler.ts
+++ src/infrastructure/externals/exchange-rates/market-status-scheduler.ts
@@ -1,25 +1,27 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { ExternalWebSocketGateWay } from '../external-websocket.gateway';
+import { IExchangeRateWebSocketService } from './interfaces/exchange-rate-websocket.interface';
 
 @Injectable()
 export class MarketStatusScheduler {
   private readonly logger = new Logger(MarketStatusScheduler.name);
   constructor(
-    private readonly externalWebsocketGateWay: ExternalWebSocketGateWay,
+    @Inject('WEBSOCKET_IMPL')
+    private readonly collectLatestRateSocket: IExchangeRateWebSocketService,
   ) {}
 
   // 매주 일요일 21시 정각 (UTC)
   @Cron('0 21 * * 0')
   handleMarketOpen() {
-    this.externalWebsocketGateWay.testConnectLatestRateSocket();
+    // this.externalWebsocketGateWay.testConnectLatestRateSocketㅇ();
+    this.collectLatestRateSocket.connect();
     this.logger.debug('시장 개장시간이라 latestRate 소켓연결을 시작합니다.');
   }
 
   //   매주 금요일 21시 정각
   @Cron('0 21 * * 5')
   handleMarketClose() {
-    this.externalWebsocketGateWay.disconnectLatestRateSocket();
+    this.collectLatestRateSocket.disconnect();
     this.logger.debug('시장 마감시간이라 latestRate 소켓연결을 해제합니다.');
   }
 }
