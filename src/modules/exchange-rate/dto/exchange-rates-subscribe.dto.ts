bbc66da70db644e917e575fdeb8f5d62cabef37b
DongSeonYoo <inko51366@naver.com>
2025-06-09 15:04:54 +0900

    refactor: add validation for KRW only (baseCurrency)

--- src/modules/exchange-rate/dto/exchange-rates-subscribe.dto.ts
+++ src/modules/exchange-rate/dto/exchange-rates-subscribe.dto.ts
@@ -1,9 +1,9 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Matches } from 'class-validator';
 import { IsValidCurrencyCode } from '../../../common/decorators/validations/is-valid-currency.validator';
 
 export class ExchangeRateSubscribeDto {
   @IsNotEmpty()
-  @IsValidCurrencyCode()
+  @Matches(/^KRW$/, { message: 'baseCurrency is only can be KRW.' })
   baseCurrency: string;
 
   @IsNotEmpty()
