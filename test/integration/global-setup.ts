9ddf00484997da23013e1c34fdb51a6164ffb04c
DongSeonYoo <inko51366@naver.com>
2025-06-22 20:32:57 +0900

    refactor: unnecessary import pruning

--- test/integration/global-setup.ts
+++ test/integration/global-setup.ts
@@ -12,13 +12,6 @@ module.exports = async () => {
   console.log('✅ [Global Setup] Running prisma migrate reset...');
   execSync(
     `npx prisma migrate reset --force --schema=${join(backendDir, 'prisma', 'schema.prisma')}`,
-    {
-      env: {
-        ...process.env,
-        DATABASE_URL:
-          'postgresql://postgres:postgres@localhost:5432/exchange_watch', // 개발용도라 노출상관없을듯
-      },
-    },
   );
 
   console.log('✅ [Global Setup] Done.');
