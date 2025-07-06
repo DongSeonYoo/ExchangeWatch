1302c34fde277619265753b4cfbce57e3f7ce8d0
DongSeonYoo <inko51366@naver.com>
2025-07-06 18:47:01 +0900

    chore: add entrypoint shell script for db migrate && historical seed

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+set -e
+
+echo "Downloading historical seed data from S3..."
+aws s3 cp s3://exchange-watch-seeds/historical-mock/ /usr/src/app/historical-mock/ --recursive
+
+echo "Running database migrations..."
+npx prisma migrate deploy
+
+# start database seeding
+# using historical-mock json files
+echo "Running database seeding..."
+npm run seed:prod
+
+echo "Starting application..."
+exec "$@"
